99dd02ef50486dd1d8544fcd1936922029b48b82
obrana <ome.bikram@gmail.com>
2017-05-16 11:08:40 +0200

    Add project files.

---
+++ OstSysMVVM/Handler/ApartmentHandler.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OstSysMVVM.Model;
+using OstSysMVVM.Persistency;
+using OstSysMVVM.ViewModel;
+
+namespace OstSysMVVM.Handler
+{
+    class ApartmentHandler
+    {
+        public ApartmentViewModel ApartmentViewModel { get; set; }
+
+        public ApartmentHandler(ApartmentViewModel apartmentViewModel)
+        {
+            ApartmentViewModel = apartmentViewModel;
+        }
+
+        public void CreateApartment()
+        {
+            Apartment aApartment = new Apartment();
+            aApartment.Apartment_ID = ApartmentViewModel.NewApartment.Apartment_ID;
+            aApartment.Address = ApartmentViewModel.NewApartment.Address;
+            aApartment.BBR = ApartmentViewModel.NewApartment.BBR;
+            aApartment.Number_Of_Rooms = ApartmentViewModel.NewApartment.Number_Of_Rooms;
+            aApartment.Floor = ApartmentViewModel.NewApartment.Floor;
+            aApartment.Foredelingstal = ApartmentViewModel.NewApartment.Foredelingstal;
+            aApartment.Side = ApartmentViewModel.NewApartment.Side;
+            aApartment.Size = ApartmentViewModel.NewApartment.Size;
+            aApartment.Reserved = ApartmentViewModel.NewApartment.Reserved;
+
+          new PersistenceFacade().SaveApartment(aApartment);
+
+
+
+
+        }
+     
+    }
+}
---
+++ OstSysMVVM/Persistency/PersistenceFacade.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Popups;
+using Newtonsoft.Json;
+using OstSysMVVM.Model;
+
+namespace OstSysMVVM.Persistency
+{
+    class PersistenceFacade
+    {
+        const string ServerUrl = "http://localhost:61359";
+        HttpClientHandler handler;
+
+        public PersistenceFacade()
+        {
+            handler = new HttpClientHandler();
+            handler.UseDefaultCredentials = true;
+        }
+
+        public List<Apartment> GetApartments()
+        {
+            using (var client = new HttpClient(handler))
+            {
+                client.BaseAddress = new Uri(ServerUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                try
+                {
+                    var response = client.GetAsync("api/Apartments").Result;
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var apartmentlist = response.Content.ReadAsAsync<IEnumerable<Apartment>>().Result;
+                        return apartmentlist.ToList();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    new MessageDialog(ex.Message).ShowAsync();
+                }
+                return null;
+            }
+        }
+
+        public void SaveApartment(Apartment aApartment)
+        {
+            using (var client = new HttpClient(handler))
+            {
+                client.BaseAddress = new Uri(ServerUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                try
+                {
+                    string postBody = JsonConvert.SerializeObject(aApartment);
+                    //var response = 
+                    var response =
+                        client.PostAsync("api/apartments", new StringContent(postBody, Encoding.UTF8, "application/json"))
+                            .Result;
+                }
+                catch (Exception ex)
+                {
+                    new MessageDialog(ex.Message).ShowAsync();
+
+                }
+            }
+
+
+        }
+    }
+}
